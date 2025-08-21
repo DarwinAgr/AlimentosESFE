4d96f4023b1b872db7c7c24891700a36ac6eaf84
DarwinAgr <aguilardarwin3064@gmail.com>
2025-08-21 09:57:30 -0600

    Creacion de la estructura CleanArquitecture

---
+++ src/main/java/org/esfe/AlimentosESFE/AlimentosEsfeApplication.java
@@ -0,0 +1,13 @@
+package org.esfe.AlimentosESFE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlimentosEsfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlimentosEsfeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/esfe/AlimentosESFE/AlimentosEsfeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esfe.AlimentosESFE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlimentosEsfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
